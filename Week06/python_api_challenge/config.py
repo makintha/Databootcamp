687b139bbd48c1e260b6a22e32b050522a92f5b6
Ahmad Makintha Brany <makintha@gmail.com>
2021-04-26 12:16:46 +0800

    Upload Week06 API Challenge

---
+++ Week06/python_api_challenge/config.py
@@ -0,0 +1,4 @@
+# api_key = "50d27b4a688f99f300e1021533857817"
+# api_key = "4c28215f1c71887e12ca92c10a6ac317"
+api_key = "a8b92cad2af07cc06ed21a5015fef1cd"
+g_key = 'AIzaSyDKzGh1_PhGU--liKCpPWg1fQfDF76PSxg'
